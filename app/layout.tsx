f7dc23db3b79995dbe627549846416af6854e18c
david.chien <f312213213david@gmail.com>
2024-01-02 14:27:26 +0800

    feat: implement personal website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Command from "@/components/cmdk";
-import { ModeToggle, ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
+import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -15,14 +16,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <body className={cn(inter.className)}>
+    <html
+      lang="en"
+      style={{ colorScheme: "light" }}
+      className={cn(inter.className, "light")}
+    >
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <ModeToggle />
           <Command />
           {children}
-        </body>
-      </ThemeProvider>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
