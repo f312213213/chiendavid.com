7c5b350dd8d6c30b85903747d2191c88fb1dfcdb
f312213213 <f312213213david@gmail.com>
2025-07-21 10:18:52 +0800

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import Footer from "./components/Footer";
 
 const inter = Inter({
@@ -24,6 +26,23 @@ export default function RootLayout({
       <body
         className={`${inter.variable} antialiased`}
       >
+        <Script id="woopra-analytics" strategy="afterInteractive">
+          {`
+            (function(){
+              var t,o,c,e=window,n=document,r=arguments,a="script",i=["call","cancelAction","config","identify","push","track","trackClick","trackForm","update","visit"],s=function(){var t,o=this,c=function(t){o[t]=function(){return o._e.push([t].concat(Array.prototype.slice.call(arguments,0))),o}};for(o._e=[],t=0;t<i.length;t++)c(i[t])};for(e.__woo=e.__woo||{},t=0;t<r.length;t++)e.__woo[r[t]]=e[r[t]]=e[r[t]]||new s;(o=n.createElement(a)).async=1,o.src="https://static.woopra.com/js/w.js",(c=n.getElementsByTagName(a)[0]).parentNode.insertBefore(o,c)
+            })("woopra");
+
+            window.woopra = window.woopra || {};
+            window.woopra.config && window.woopra.config({
+              domain: "chiendavid.com",
+              outgoing_tracking: true,
+              download_tracking: true,
+              click_tracking: true
+            });
+            window.woopra.track && window.woopra.track();
+          `}
+        </Script>
+        
         <script dangerouslySetInnerHTML={{
           __html: `
             (function() {
@@ -43,6 +62,7 @@ export default function RootLayout({
         {children}
 
         {/* <Footer /> */}
+        <GoogleAnalytics gaId="G-TP0XQPDH1G" />
       </body>
     </html>
   );
